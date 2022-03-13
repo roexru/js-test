b5ebe2009d8430fda0c27cf012ada04258279468
Roman <o18brv30s.@yandex.ru>
2022-03-13 14:31:08 +1000

    first js task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = prompt('Скольлко фильмов вы уже смотрели?', '');
+
+const personalMuvieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private:false
+};
+
+const a = prompt('Один из просмотренных ранее фильмов?', ''),
+		b = prompt('На сколько оцените его?' ,''),
+		c = prompt('Один из просмотренных ранее фильмов?', ''),
+		d = prompt('На сколько оцените его?' ,'');
+
+personalMuvieDB.movies[a] = b;
+personalMuvieDB.movies[c] = d;
+
+console.log(personalMuvieDB);
