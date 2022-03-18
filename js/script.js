0807979ff86288f426724e2267556cf03eaa66b7
roex <o18brv30s@gmail.com>
2022-03-18 21:58:09 +1000

    loop

--- js/script.js
+++ js/script.js
@@ -11,11 +11,33 @@ const personalMuvieDB = {
 };
 
 const a = prompt('Один из просмотренных ранее фильмов?', ''),
-		b = prompt('На сколько оцените его?' ,''),
-		c = prompt('Один из просмотренных ранее фильмов?', ''),
-		d = prompt('На сколько оцените его?' ,'');
+	  b = prompt('На сколько оцените его?' ,''),
+	  c = prompt('Один из просмотренных ранее фильмов?', ''),
+	  d = prompt('На сколько оцените его?' ,'');
 
 personalMuvieDB.movies[a] = b;
 personalMuvieDB.movies[c] = d;
 
-console.log(personalMuvieDB);
+for (let i =0; i < 2; i++) {
+	const a = prompt('Один из просмотренных ранее фильмов?', ''),
+	      b = prompt('На сколько оцените его?' ,'');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMuvieDB.movies[a] = b;
+		console.log(done);
+	} else {
+		console.log('error');
+		i--;
+	}
+	
+}
+
+if(personalMuvieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMuvieDB.count >= 10 && personalMuvieDB.count < 30) {
+	console.log('Вы классический зритель');	
+} else if (personalMuvieDB.count >= 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
